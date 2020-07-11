0508919789391867d8c0fc95ffa3581a174f4416
stany <2001stany@gmail.com>
2020-07-11 09:44:44 -0400

    add env file

--- NotesForAll/NotesForAll/settings.py
+++ NotesForAll/NotesForAll/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xr*v$*yky)l$&3xvn&vqd_m2dg$*qg(fl1sxdxaj5_6d1!l1ln'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
